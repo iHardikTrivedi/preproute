2812d2246a903b6f18b212f77e9bb21cd111eb36
Hardik Trivedi <ihardiktrivedi.ihart@gmail.com>
2026-07-06 15:08:08 +0530

    vercel setup

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,8 +3,7 @@ export const ENV = {
 
   APP_ENV: import.meta.env.VITE_APP_ENV || "development",
 
-  API_URL:
-    import.meta.env.VITE_API_URL || "https://admin-moderator-backend-staging.up.railway.app/api",
+  API_URL: import.meta.env.VITE_API_URL || "/api",
 
   APP_VERSION: import.meta.env.VITE_APP_VERSION || "1.0.0",
 } as const;
